037021e7320cf1cf9f1befcd6751d80635b38587
epiliper <epiliper@outlook.com>
2024-06-20 15:00:52 -0700

    creates one col per ref coordinate

--- pull_umis/src/main.rs
+++ pull_umis/src/main.rs
@@ -1,18 +1,38 @@
 use bam::{BamReader, Record};
-use std::fs::{File, OpenOptions};
+use std::fs::File;
 use std::path::Path;
-
+use polars::prelude::*;
+use polars::functions::concat_df_horizontal;
 use clap::Parser;
-use std::io::Write;
 
 use indexmap::IndexMap;
+
+fn main() {
+    let args = Args::parse();
+    let input = args.file;
+
+    let bam_file = BamReader::from_path(&input, 8).unwrap();
+
+    let outfile = format!("{}{}", input.split('.').next().unwrap(), "_umis.csv").to_string();
+    let outfile = Path::new(&outfile);
+    let _ = File::create(outfile);
+
+    let mut umis: IndexMap<i32, Vec<String>> = IndexMap::new();
+
+    for r in bam_file {
+        let read = &r.unwrap();
+        pull_umi(read, &mut umis, &args.separator)
+    }
+
+    write_tsv(umis, &outfile);
+}
 
 fn get_umi(record: &Record, separator: &String) -> String {
     let umi = String::from_utf8(record.name().to_vec());
     umi.unwrap().split(separator).last().unwrap().to_string()
 }
 
-pub fn pull_read(read: &Record, store: &mut IndexMap<i32, Vec<String>>, separator: &String) {
+pub fn pull_umi(read: &Record, store: &mut IndexMap<i32, Vec<String>>, separator: &String) {
     // if read is reverse to reference, group it by its last aligned base to the reference
     if read.flag().is_mapped() && read.flag().is_reverse_strand() {
         let pos = read.calculate_end() + 1;
@@ -27,22 +47,27 @@ pub fn pull_read(read: &Record, store: &mut IndexMap<i32, Vec<String>>, separato
     }
 }
 
-pub fn write_umis(position: i32, umis: Vec<String>, outfile: &Path) {
+pub fn write_tsv(store: IndexMap<i32, Vec<String>>, outfile: &Path) {
 
-    let mut f = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .open(&outfile)
-        .expect("unable to open file");
+    let mut file = File::create(outfile).expect("Could not create file!");
 
-    f.write(&format!("{}\t", position).into_bytes());
+    let mut dfs: Vec<DataFrame> = Vec::new();
 
-    umis.iter().for_each(
-        |umi| {
-            f.write(&format!("{}\t", umi).into_bytes());
-        });
+    for (pos, umis) in store {
 
-    f.write(b"\n");
+        dfs.push(
+            DataFrame::new(vec![Series::new(&pos.to_string(), umis)]).unwrap(),
+        );
+    }
+
+    let mut new_report = concat_df_horizontal(&dfs).unwrap();
+    new_report.align_chunks();
+
+    CsvWriter::new(&mut file)
+        .n_threads(8)
+        .finish(&mut new_report)
+        .unwrap();
+
 }
 
 #[derive(Parser, Debug)]
@@ -51,29 +76,4 @@ struct Args {
     file: String,
     #[arg(short = 's')]
     separator: String,
-}
-
-fn main() {
-    let args = Args::parse();
-    let input = args.file;
-
-    let mut bam_file = BamReader::from_path(&input, 8).unwrap();
-
-    let outfile = format!("{}{}", input.split('.').next().unwrap(), "_umis.tsv").to_string();
-    let mut outfile = Path::new(&outfile);
-    File::create(outfile);
-
-    let mut umis: IndexMap<i32, Vec<String>> = IndexMap::new();
-
-    for r in bam_file {
-        let read = &r.unwrap();
-        pull_read(read, &mut umis, &args.separator)
-    }
-
-    umis.drain(0..).for_each(
-        |position| {
-            write_umis(position.0, position.1, outfile)
-        }
-    )
-
 }
