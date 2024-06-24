3643a495d80355b4cf3150406ac36dfbb4d5a26b
epiliper <epiliper@outlook.com>
2024-06-24 09:57:36 -0700

    writes out raw edit distances

--- pull_umis/src/main.rs
+++ pull_umis/src/main.rs
@@ -7,8 +7,8 @@ use rand::rngs::StdRng;
 use rayon::prelude::*;
 use std::fs::File;
 use std::path::Path;
+use std::sync::{Arc, Mutex};
 use strsim::hamming;
-use std::sync::{Mutex, Arc};
 
 use rand::seq::SliceRandom;
 use rand::SeedableRng;
@@ -108,34 +108,59 @@ pub fn write_umis(mut store: IndexMap<i32, Vec<String>>, outfile: &Path, sample_
         .unwrap();
 }
 
-pub fn write_edit_distance(
-    mut store: IndexMap<i32, Vec<String>>,
-    outfile: &Path,
-    sample_size: usize,
-) {
-
-
+pub fn write_means(mut store: IndexMap<i32, Vec<String>>, outfile: &Path, sample_size: usize) {
     let mut file = File::create(outfile).expect("Could not create file!");
     let mut dfs: Arc<Mutex<Vec<DataFrame>>> = Arc::new(Mutex::new(Vec::new()));
 
     store.par_drain(0..).for_each(|(pos, umi_list)| {
         let mut rng = StdRng::seed_from_u64(5);
         let mut edits: Vec<usize> = Vec::new();
-        let sample = umi_list.choose_multiple(&mut rng, sample_size).collect::<Vec<&String>>();
+        let sample = umi_list
+            .choose_multiple(&mut rng, sample_size)
+            .collect::<Vec<&String>>();
 
         let mut i = 0;
         for umi in &sample {
-            for neighbor in &sample[i+1..] {
+            for neighbor in &sample[i + 1..] {
                 edits.push(hamming(&umi, neighbor).unwrap())
             }
             i += 1;
         }
         if !edits.is_empty() {
-        let df = DataFrame::new(
-            vec![Series::new(&pos.to_string(), [mean(edits) as f32])]
-        ).unwrap();
+            let df =
+                DataFrame::new(vec![Series::new(&pos.to_string(), [mean(edits) as f32])]).unwrap();
 
-        dfs.lock().unwrap().push(df);
+            dfs.lock().unwrap().push(df);
+        }
+    });
+}
+
+pub fn write_edit_distance(
+    mut store: IndexMap<i32, Vec<String>>,
+    outfile: &Path,
+    sample_size: usize,
+) {
+    let mut file = File::create(outfile).expect("Could not create file!");
+    let mut dfs: Arc<Mutex<Vec<DataFrame>>> = Arc::new(Mutex::new(Vec::new()));
+
+    store.par_drain(0..).for_each(|(pos, umi_list)| {
+        let mut rng = StdRng::seed_from_u64(5);
+        let mut edits: Vec<i32> = Vec::new();
+        let sample = umi_list
+            .choose_multiple(&mut rng, sample_size)
+            .collect::<Vec<&String>>();
+
+        let mut i = 0;
+        for umi in &sample {
+            for neighbor in &sample[i + 1..] {
+                edits.push(hamming(&umi, neighbor).unwrap() as i32)
+            }
+            i += 1;
+        }
+        if !edits.is_empty() {
+            let df = DataFrame::new(vec![Series::new(&pos.to_string(), edits)]).unwrap();
+
+            dfs.lock().unwrap().push(df);
         }
     });
 
@@ -149,6 +174,5 @@ pub fn write_edit_distance(
 }
 
 fn mean(list: Vec<usize>) -> f32 {
-
-    return list.iter().sum::<usize>() as f32 / list.len() as f32
+    return list.iter().sum::<usize>() as f32 / list.len() as f32;
 }
